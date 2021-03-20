1bf3878f8cd17f5f393589e8d629b88f6a320635
A01749448 <a01749448@itesm.mx>
2021-03-19 21:59:00 -0600

    Escena menú, botones

--- Assets/Scripts/Moneda.cs
+++ Assets/Scripts/Moneda.cs
@@ -2,6 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Detecta la colision con una moneda
+
+Autores:   
+Jorge Chávez Badillo            A01749448
+Ariadna Jocelyn Guzmán Jiménez  A01749373
+Liam Garay Monroy               A01750632
+Amy Murakami Tsutsumi           A01750185
+Andrea Vianey Díaz Álvarez      A01750147
+*/
+
 public class Moneda : MonoBehaviour
 {
    private void OnTriggerEnter2D(Collider2D other)
--- Assets/Scripts/MoverPersonaje.cs
+++ Assets/Scripts/MoverPersonaje.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+
+Autores: 
+Jorge Chávez Badillo            A01749448
+Ariadna Jocelyn Guzmán Jiménez  A01749373
+Liam Garay Monroy               A01750632
+Amy Murakami Tsutsumi           A01750185
+Andrea Vianey Díaz Álvarez      A01750147
+*/
+
 public class MoverPersonaje : MonoBehaviour
 {
     //VARIABLES
