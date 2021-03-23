94855b838bbcbd312a503d23dff81323f6f84f8f
amymurakami17 <A01750185@itesm.mx>
2021-03-23 16:46:16 -0600

    Enemigos
    Se agregaron los 4 enemigos con el script

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*
+ * Detecta la colisión del enemigo con el personaje
+ * 
+ * Autores: 
+ * Jorge Chávez Badillo A01749448
+ * Ariadna Jocelyn Guzmán Jiménez A01749373
+ * Liam Garay Monroy A01750632
+ * Andrea Vianey Díaz Álvarez A01750147
+ * Amy Murakami Tsutsumi A01750185
+ */
+public class Enemigo : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Descontar una vida 
+            //SaludPersonaje.instance.vidas--; 
+            //Actualizar los 'corazones'
+            //HUD.instance.ActualizarVidas();
+            //if (SaludPersonaje.instance.vidas == 0)
+            {
+                Destroy(other.gameObject, 0.3f);
+                //SceneManager.LoadScene("EscenaMenu"); //Pierde, regresa al menú
+            }
+        }
+    }
+}
