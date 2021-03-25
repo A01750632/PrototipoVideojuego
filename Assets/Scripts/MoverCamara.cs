34b71e7ca5e38062e908783aa940ed9ae12c0946
a01750147 <A01750147@itesm.mx>
2021-03-25 12:04:47 -0600

    Cambios

--- Assets/Scripts/MoverCamara.cs
+++ Assets/Scripts/MoverCamara.cs
@@ -17,7 +17,7 @@ public class MoverCamara : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Mathf.Clamp(personaje.transform.position.x, min: -3.5f, max: 55.4f);
+        float x = Mathf.Clamp(personaje.transform.position.x, min: -3.2f, max: 55.4f);
         float y = Mathf.Clamp(personaje.transform.position.y, min: 0, max: 6.6f);
         float z = transform.position.z;
         transform.position = new Vector3(x, y, z);
