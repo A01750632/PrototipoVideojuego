04fa14e7cbc3b9a19fc18748c58d4a4f69e09bfe
A01749448 <a01749448@itesm.mx>
2021-04-16 19:05:30 -0500

    Nivel 4

--- Assets/Scripts/Red.cs
+++ Assets/Scripts/Red.cs
@@ -14,18 +14,18 @@ using UnityEngine.SceneManagement;
 
 public class Red : MonoBehaviour
 {
-    //Para desplegar la información
+    //Para desplegar la informaciï¿½n
     public Text resultado;
 
     public static Red instance;
     public static String tiempoInicio;
 
-    //Campos con la información nombre y puntos
+    //Campos con la informaciï¿½n nombre y puntos
     public Text textoNombre;
-    public Text textoContraseña;
+    public Text textoContrasenia;
     public static String nombre;
     //Escribir
-    public void EscribirTextoPlano()     //Botón
+    public void EscribirTextoPlano()     //Botï¿½n
     {
         //Concurrente
         StartCoroutine(SubirTextoPlano());
@@ -33,15 +33,15 @@ public class Red : MonoBehaviour
 
     private IEnumerator SubirTextoPlano()
     {
-        //Encapsular los datos que se suben a la red con el método POST
+        //Encapsular los datos que se suben a la red con el mï¿½todo POST
         WWWForm forma = new WWWForm();
 
         forma.AddField("usuarioUsuarioo", textoNombre.text);
-        forma.AddField("passwordUsuarioo", textoContraseña.text);
+        forma.AddField("passwordUsuarioo", textoContrasenia.text);
 
         UnityWebRequest request = UnityWebRequest.Post("http://Localhost:8080/BuscarUsuario", forma); //
         yield return request.SendWebRequest();   //Regresa, ejecuta, espera...
-        //...ya regresó a la línea 27 (terminó de ejecutar SendWebRequest())
+        //...ya regresï¿½ a la lï¿½nea 27 (terminï¿½ de ejecutar SendWebRequest())
 
         if (request.result == UnityWebRequest.Result.Success)  //200 OK
         {
